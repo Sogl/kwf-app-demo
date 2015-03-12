400730bbb88b5330d1cf2b053e0d9d0e643ce3ec
Sogl <artyom89@inbox.ru>
2015-03-12 17:15:01 +1000

    Call json-xls from custom dropdown

--- controllers/Members/MembersController.php
+++ controllers/Members/MembersController.php
@@ -4,7 +4,7 @@ class Members_MembersController extends Kwf_Controller_Action_Auto_Grid
     protected $_modelName = 'Members';
     protected $_defaultOrder = 'lastname';
     protected $_paging = 20;
-    protected $_buttons = array('add');
+    protected $_buttons = array('add', 'xls');
 	protected $_editDialog = array(
                                      'controllerUrl' => '/members/memberform',
                                      'width' => 550,
@@ -20,7 +20,7 @@ class Members_MembersController extends Kwf_Controller_Action_Auto_Grid
     protected function _initColumns()
     {
         $this->_filters = array('text' => array('type' => 'TextField'));
-		
+
 		$this->_columns->add(new Kwf_Grid_Column_Button('edit'));
         $this->_columns->add(new Kwf_Grid_Column('lastname', trl('Lastname'), 140));
         $this->_columns->add(new Kwf_Grid_Column('firstname', trl('Firstname')));
--- js/Members.js
+++ js/Members.js
@@ -20,11 +20,33 @@ var Members = Ext2.extend(Ext2.Panel,
         var grid = new Kwf.Auto.GridPanel({
             controllerUrl   : '/members/members',
             region          : 'west',
-            width           : 300,
+            width           : 700,
             resizable       : true,
             split           : true,
             collapsible     : true,
             title           : trl('Customers'),
+            gridConfig: {
+                tbar: [
+                    //'->',
+                    {
+                        text:'Выгрузки',
+                        icon: '/assets/silkicons/page_excel.png',
+                        cls: 'x2-btn-text-icon',
+                        menu: [{
+                            text:'Выгрузка EXXON',
+                            icon: '/assets/silkicons/page_excel.png',
+                            cls: 'x2-btn-text-icon',
+                            handler : this.onXls
+                        },{
+                            text:'Выгрузка SEIC',
+                            icon: '/assets/silkicons/page_excel.png',
+                            cls: 'x2-btn-text-icon',
+                            handler : this.onXls
+                        }
+                        ]
+                    },
+                ]
+            },
             bindings: [form, {
                 queryParam: 'member_id',
                 item: contacts
@@ -38,5 +60,44 @@ var Members = Ext2.extend(Ext2.Panel,
             items: [form, contacts]
         }];
         Members.superclass.initComponent.call(this);
+    },
+
+    onXls : function() {
+        var params = Kwf.clone(this.getStore().baseParams);
+        if(this.getStore().sortInfo){
+            var pn = this.getStore().paramNames;
+            params[pn["sort"]] = this.getStore().sortInfo.field;
+            params[pn["dir"]] = this.getStore().sortInfo.direction;
+        }
+
+        Ext2.Ajax.request({
+            url : this.controllerUrl+'/json-xls',
+            params  : params,
+            timeout: 600000, // 10 minuten
+            progress: true,
+            progressTitle : trlKwf('Excel export'),
+            success: function(response, opt, r) {
+                var downloadUrl = this.controllerUrl+'/download-export-file?downloadkey='+r.downloadkey;
+                for (var i in params) {
+                    downloadUrl += '&' + i + '=' + params[i];
+                }
+                if (Ext2.isIE) {
+                    Ext2.Msg.show({
+                        title: trlKwf('Your download is ready'),
+                        msg: trlKwf('Please click on the following link to download your Excel file.')
+                            +'<br /><br />'
+                            +'<a class="xlsExportLink" href="'+downloadUrl+'" target="_blank">'
+                            +trlKwf('Excel export file')+'</a>',
+                        icon: Ext2.Msg.INFO,
+                        buttons: { ok: trlKwf('Close') }
+                    });
+                } else {
+                    Ext2.getBody().createChild({
+                        html: '<iframe width="0" height="0" src="'+downloadUrl+'"></iframe>'
+                    });
+                }
+            },
+            scope: this
+        });
     }
 });
